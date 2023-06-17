7ac7967c2fd5d253b78eea20ab70895208edc88b
mentadoje <mentadoje@gmail.com>
2023-06-17 18:57:19 -0300

    Assignment completed. Letter Grader

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Write your grade percentage: ");
+        string gradeObtained = Console.ReadLine();
+
+        int gradeValue = int.Parse(gradeObtained);
+        string letterGrade = "";
+
+        if (gradeValue >= 90 )
+        {
+            letterGrade = "A";
+        }
+        else if (gradeValue >= 80 )
+        {
+            letterGrade = "B";
+        }
+        else if (gradeValue >= 70 )
+        {
+            letterGrade = "C";
+        }
+        else if (gradeValue >= 60 )
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        
+        Console.WriteLine($"Your grade is: {letterGrade}");
+        
+        
+        if (gradeValue >= 70)
+        {
+            Console.WriteLine("You have approved this course. Congrats!! ");
+        }
+        else
+        {
+            Console.WriteLine("You didn't meet the requirements to aprove the course. Try again next term!");
+        }
     }
 }
