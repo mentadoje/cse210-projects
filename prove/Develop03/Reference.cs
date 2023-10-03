681c7fa772ea4dcebc30e5ff8779f6b6136b36d4
mentadoje <mentadoje@gmail.com>
2023-10-03 16:13:58 -0300

    Classes and methods created and organized

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Data;
+
+public class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+
+    }
+
+    public Reference(string book, int chapter, int startVerse, int _endVerse)
+    {
+
+    }
+
+
+
+    public string GetDisplayText()
+    {
+        return "" ;
+    }
+
+
+}
